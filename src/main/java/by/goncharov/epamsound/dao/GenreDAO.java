32956a8330178819641ad5014093a7ba1c098083
Daniil Goncharov <brovar002@gmail.com>
2021-11-02 20:19:53 +0300

    feat: add DAO for Order and Genre

---
+++ src/main/java/by/goncharov/epamsound/dao/GenreDAO.java
@@ -0,0 +1,68 @@
+package by.goncharov.epamsound.dao;
+
+import by.goncharov.epamsound.manager.ProxyConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreDAO extends AbstractDAO {
+    private static final String SQL_INSERT_GENRE = "INSERT INTO"
+            + " genre (`genre`) VALUES (?);";
+    private static final String SQL_SELECT_GENRE_ID = "SELECT id"
+            + " FROM genre WHERE genre=?";
+    private static final String SQL_SELECT_GENRES = "SELECT `genre` FROM genre";
+    public GenreDAO(final ProxyConnection connection) {
+        super(connection);
+    }
+    private int addGenre(final String genre) throws DAOException {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_INSERT_GENRE);
+            statement.setString(1, genre.toUpperCase());
+            statement.executeUpdate();
+            return findGenreId(genre);
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        } finally {
+            closeStatement(statement);
+        }
+    }
+    public int findGenreId(final String genre) throws DAOException {
+        int id;
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_GENRE_ID);
+            statement.setString(1, genre.toUpperCase());
+            ResultSet set = statement.executeQuery();
+            if (set.next()) {
+                id = set.getInt("id");
+            } else {
+                id = addGenre(genre);
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        } finally {
+            closeStatement(statement);
+        }
+        return id;
+    }
+    public List<String> findGenres() throws DAOException {
+        List<String> genreList = new ArrayList<>();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_GENRES);
+            ResultSet set = statement.executeQuery();
+            while (set.next()) {
+                genreList.add(set.getString("genre"));
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        } finally {
+            closeStatement(statement);
+        }
+        return genreList;
+    }
+
+}
---
+++ src/main/java/by/goncharov/epamsound/dao/OrderDAO.java
@@ -0,0 +1,62 @@
+package by.goncharov.epamsound.dao;
+
+import by.goncharov.epamsound.beans.Track;
+import by.goncharov.epamsound.manager.ProxyConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class OrderDAO extends AbstractDAO {
+    private final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String SQL_ADD_ORDER = "INSERT INTO"
+            + " `order`(price,`date` ,user_id, audio_track_id)"
+            + " VALUES(?,?,?,?);";
+    private static final String SQL_SELECT_USER_ORDERS = "SELECT"
+            + " audio_track.id, audio_track.name, genre.genre, "
+            + "audio_track.artist_name, audio_track.price FROM `order`\n"
+            + "JOIN audio_track ON audio_track.id=`order`.audio_track_id\n"
+            + "LEFT JOIN genre ON audio_track.genre_id=genre.id\n"
+            + "WHERE `order`.user_id=?\n"
+            + "ORDER BY audio_track.name";
+    public OrderDAO(final ProxyConnection connection) {
+        super(connection);
+    }
+    public void addOrder(final int trackId,
+                         final double price, final int userId)
+            throws DAOException {
+        PreparedStatement statement = null;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.
+                    ofPattern(DATE_PATTERN);
+            LocalDateTime now = LocalDateTime.now();
+            String dateTime = now.format(formatter);
+            statement = connection.prepareStatement(SQL_ADD_ORDER);
+            statement.setDouble(1, price);
+            statement.setString(2, dateTime);
+            statement.setInt(3, userId);
+            statement.setInt(4, trackId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        } finally {
+            closeStatement(statement);
+        }
+    }
+    public List<Track> findOrders(final int userId) throws DAOException {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_USER_ORDERS);
+            statement.setInt(1, userId);
+            ResultSet set = statement.executeQuery();
+            TrackDAO trackDAO = new TrackDAO(connection);
+            return trackDAO.formTrackList(set);
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        } finally {
+            closeStatement(statement);
+        }
+    }
+}
