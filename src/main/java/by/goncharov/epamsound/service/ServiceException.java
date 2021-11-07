3607415f006e9cb40cfdf2fc718b57f37ed6e0c7
Daniil Goncharov <brovar002@gmail.com>
2021-11-07 18:48:35 +0300

    feat: update ServiceException

--- src/main/java/by/goncharov/epamsound/service/ServiceException.java
+++ src/main/java/by/goncharov/epamsound/service/ServiceException.java
@@ -4,5 +4,13 @@ public class ServiceException extends Exception {
     public ServiceException(final String message, final Throwable cause) {
         super(message, cause);
     }
-
+    public ServiceException(final Throwable cause) {
+        super(cause);
+    }
+    public ServiceException() {
+        super();
+    }
+    public ServiceException(final String message) {
+        super(message);
+    }
 }
