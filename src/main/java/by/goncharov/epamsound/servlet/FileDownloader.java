34d8311fb7b05a6e0a5b44b336ad6a65d3dbab94
Daniil Goncharov <brovar002@gmail.com>
2021-11-09 22:11:36 +0300

    feat: add file loaders

---
+++ src/main/java/by/goncharov/epamsound/servlet/FileDownloader.java
@@ -0,0 +1,49 @@
+package by.goncharov.epamsound.servlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FileDownloader {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final String MIME_TYPE = "application/octet-stream";
+    private static final String HEADER_KEY = "Content-Disposition";
+    private static final String ATTACHMENT = "attachment; filename=\"%s\"";
+    private static final int END = -1;
+    private static final int BUFFER_LENGTH = 4096;
+    private static final int START_OFFSET = 0;
+    boolean downloadTrack(final String filePath,
+                          final HttpServletResponse response,
+                          final ServletContext context) {
+        try {
+            File downloadFile = new File(filePath);
+            if (downloadFile.exists() && downloadFile.isFile()) {
+                FileInputStream inStream = new FileInputStream(downloadFile);
+                String mimeType = context.getMimeType(filePath);
+                if (mimeType == null) {
+                    mimeType = MIME_TYPE;
+                }
+                response.setContentType(mimeType);
+                response.setContentLength((int) downloadFile.length());
+                String headerValue = String.format(ATTACHMENT,
+                        downloadFile.getName());
+                response.setHeader(HEADER_KEY, headerValue);
+                OutputStream outStream = response.getOutputStream();
+                byte[] buffer = new byte[BUFFER_LENGTH];
+                int bytesRead;
+                while ((bytesRead = inStream.read(buffer)) != END) {
+                    outStream.write(buffer, START_OFFSET, bytesRead);
+                }
+                return true;
+            }
+        } catch (IOException e) {
+            LOGGER.error("Exception during file downloading");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/by/goncharov/epamsound/servlet/FileUploader.java
@@ -0,0 +1,57 @@
+package by.goncharov.epamsound.servlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+
+public class FileUploader {
+    static final Logger LOGGER = LogManager.getLogger();
+    private static final String SAVE_DIR = "uploadTracks";
+    private static final String PATH = "path";
+    public boolean uploadFile(final HttpServletRequest request,
+                              final ServletSessionRequestContent
+                                      servletSessionRequestContent) {
+        try {
+            String appPath = request.getServletContext().getRealPath("");
+            String savePath = appPath + File.separator + SAVE_DIR;
+
+            File fileSaveDir = new File(savePath);
+            if (!fileSaveDir.exists()) {
+                fileSaveDir.mkdir();
+                LOGGER.info("Directory " + savePath + "was created");
+            }
+
+            for (Part part : request.getParts()) {
+                if (PATH.equals(part.getName())) {
+                    String fileName = extractFileName(part);
+                    fileName = new File(fileName).getName();
+                    String path = savePath + File.separator + fileName;
+                    part.write(path);
+                    servletSessionRequestContent.setRequestAttribute(PATH,
+                            path);
+                }
+            }
+            LOGGER.info("Upload has been done successfully!");
+            return true;
+        } catch (ServletException | IOException e) {
+            LOGGER.error("Exception during uploading file to server", e);
+            return false;
+        }
+    }
+
+    private String extractFileName(final Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length() - 1);
+            }
+        }
+        return "";
+    }
+}
