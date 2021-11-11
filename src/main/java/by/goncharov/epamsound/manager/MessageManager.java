35a77b888b9a57e607a575282ae44087ad9c8d17
Daniil Goncharov <brovar002@gmail.com>
2021-11-11 13:42:53 +0300

    feat: update messages

--- src/main/java/by/goncharov/epamsound/manager/MessageManager.java
+++ src/main/java/by/goncharov/epamsound/manager/MessageManager.java
@@ -3,10 +3,31 @@ package by.goncharov.epamsound.manager;
 import java.util.ResourceBundle;
 
 public class MessageManager {
+    public static final String ADD_COMMENT_ERROR = "message.add.comment";
     public static final String ADD_TRACK_DATA_ERROR =
             "message.add.track.data.error";
+    public static final String CHANGE_CASH_ERROR = "message.change.cash";
+    public static final String CHANGE_EMAIL_ERROR =
+            "message.change.email.error";
+    public static final String CHANGE_LOGIN_ERROR =
+            "message.change.login.error";
+    public static final String CHANGE_PASS_EQUAL_ERROR =
+            "message.change.pass.equal";
+    public static final String CHANGE_PASS_EQUAL_NEW_ERROR =
+            "message.change.pass.equal.new";
+    public static final String CHANGE_PASS_NEW_ERROR =
+            "message.change.pass.new";
+    public static final String CHANGE_TRACK_ARTIST_ERROR =
+            "message.change.track.artist.error";
+    public static final String CHANGE_TRACK_GENRE_ERROR =
+            "message.change.track.genre.error";
+    public static final String CHANGE_TRACK_NAME_ERROR =
+            "message.change.track.name.error";
+    public static final String CHANGE_TRACK_PRICE_ERROR =
+            "message.change.track.price.error";
     public static final String NOT_UNIQUE_EMAIL = "message.signup.unique.email";
     public static final String NOT_UNIQUE_LOGIN = "message.signup.unique.login";
+    public static final String SET_BONUS_ERROR = "message.bonus.error";
     public static final String SIGNUP_ERROR = "message.signup.error";
     private  ResourceBundle resourceBundle;
     public String getProperty(final String key) {
