f618007eb3458cad5cf61ea14ad7e3d91fc029ad
Daniil Goncharov <brovar002@gmail.com>
2021-11-13 17:43:45 +0300

    feat: add AdminTag

---
+++ src/main/java/by/goncharov/epamsound/tag/AdminTag.java
@@ -0,0 +1,23 @@
+package by.goncharov.epamsound.tag;
+
+import by.goncharov.epamsound.beans.User;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class AdminTag extends TagSupport {
+    private static final String USER_ATTR = "user";
+    @Override
+    public int doStartTag() throws JspException {
+        User user = (User) pageContext.getSession().getAttribute(USER_ATTR);
+        if (user != null) {
+            if (user.getRole() == 1) {
+                return EVAL_BODY_INCLUDE;
+            } else {
+                return SKIP_BODY;
+            }
+        } else {
+            return SKIP_BODY;
+        }
+    }
+}
