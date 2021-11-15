28db66732bc5f736d5d876bcdd45f5f65f33d45c
Daniil Goncharov <brovar002@gmail.com>
2021-11-15 18:13:55 +0300

    feat: add other tags

---
+++ src/main/java/by/goncharov/epamsound/tag/NotDeletedTag.java
@@ -0,0 +1,17 @@
+package by.goncharov.epamsound.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class NotDeletedTag extends TagSupport {
+    private static final String IS_DELETED = "is_deleted";
+    @Override
+    public int doStartTag() throws JspException {
+        Object isDeleted = pageContext.getSession().getAttribute(IS_DELETED);
+        if (isDeleted == null || !Boolean.parseBoolean(isDeleted.toString())) {
+            return EVAL_BODY_INCLUDE;
+        } else {
+            return SKIP_BODY;
+        }
+    }
+}
