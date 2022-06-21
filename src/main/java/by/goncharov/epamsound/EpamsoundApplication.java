2deaaaf4ebb72fb8e315c62ebe1c0770d7a1d3b3
Daniil Goncharov <brovar002@gmail.com>
2022-06-21 22:42:05 +0300

    feat: init SpringBoot support

---
+++ src/main/java/by/goncharov/epamsound/EpamsoundApplication.java
@@ -0,0 +1,13 @@
+package by.goncharov.epamsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamsoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpamsoundApplication.class, args);
+	}
+
+}
