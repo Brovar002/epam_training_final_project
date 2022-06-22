f0b1d667b67164dbc83903a1b711a5b2bb36f0f4
Daniil Goncharov <brovar002@gmail.com>
2022-06-22 22:52:17 +0300

    refactor: set config

--- src/main/java/by/goncharov/epamsound/EpamsoundApplication.java
+++ src/main/java/by/goncharov/epamsound/EpamsoundApplication.java
@@ -2,8 +2,10 @@ package by.goncharov.epamsound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("by.goncharov.epamsound.dao")
 public class EpamsoundApplication {
 
 	public static void main(String[] args) {
