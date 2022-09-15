297063a8b9d959274029406421d3a5fd06e86824
Daniil Goncharov <brovar002@gmail.com>
2022-09-15 17:57:20 +0300

    refactor: delete unnecessary files

--- src/main/java/by/goncharov/epamsound/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package by.goncharov.epamsound.service;
-
-import by.goncharov.epamsound.beans.Order;
-
-public interface OrderService {
-    void save(Order order) throws ServiceException;
-    Order findOrderById(Order order) throws ServiceException;
-}
